40b4d98706e5ce7f04ad8f646113e6276419aebb
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-05-15 02:37:11 +0200

    Removed animations - not used

--- src/app/_animations/fade.animation.ts
+++
@@ -1,9 +0,0 @@
-import { trigger, animate, transition, style } from '@angular/animations';
-
-export const fadeAnimation = trigger('fadeInAnimation', [
-    transition(':enter', [
-      style({ opacity: 0 }),
-      animate('.3s', style({ opacity: 1 }))
-    ]),
-  ]
-);
